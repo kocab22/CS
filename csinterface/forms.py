9bf8ecb1d805188c6b02dcac28a7ddded70875ae
kocab_22 <trol@hovinko.sk>
2023-11-12 18:00:04 +0100

    crispy submit button

--- csinterface/forms.py
+++ csinterface/forms.py
@@ -1,13 +1,20 @@
 from django import forms
 from .models import PriceBid
+from django.urls import reverse_lazy
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from crispy_forms.bootstrap import InlineRadios #todo try to format inlne / how ?
+
+
 
 class NewPriceBidForm(forms.ModelForm):
-    # def __init__(self, *args, **kwargs):
-    #     initial_company = kwargs.pop('initial_company', None)  # Get the initial value for 'company'
-    #     super(NewPriceBidForm, self).__init__(*args, **kwargs)
-    #     if initial_company is not None:
-    #         self.fields['company'].initial = initial_company
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.add_input(Submit('submit', 'Uložit'))
+       
     text = forms.CharField(widget=forms.Textarea(attrs={'rows': 5, 'placeholder': 'Text k řádkům nabídky'}))
+    customer_type = forms.ChoiceField(choices=PriceBid.CustomerChoices.choices, widget=forms.RadioSelect, initial='RG')
     class Meta:
         model = PriceBid
         fields = ('number', 'parts', 'text', 'customer_type', 'company')
